4f1025e0bf9140bdc40b477bb0084e38605ac8dd
HARSH NARESHBHAI KATHIRIA <hr527292@dal.ca>
2023-05-30 18:50:45 -0300

    add folder

--- frontend/src/components/mybookings/mybookings.js
+++ frontend/src/components/mybookings/mybookings.js
@@ -2,12 +2,12 @@ import React, { Fragment } from 'react'
 import { Disclosure, Menu, Transition } from '@headlessui/react'
 import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
 import logo from '../../assets/logo.jpg';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../mybookings/mybookings.css';
 
 const navigation = [
   { name: 'Dashboard', href: '#', current: true },
-  { name: 'Team', href: '#', current: false },
-  { name: 'Projects', href: '#', current: false },
-  { name: 'Calendar', href: '#', current: false },
+  { name: 'Bookings', href: '#', current: false },
 ]
 
 function classNames(...classes) {
@@ -76,7 +76,6 @@ const people = [
 export default function Example() {
 
   return (
-    
     <div>
     <Disclosure as="nav" className="bg-gray-800">
       {({ open }) => (
@@ -207,65 +206,155 @@ export default function Example() {
       )}
     </Disclosure>
 
-
-    <div class="max-w-sm bg-white">
-    <h2 className="text-base font-semibold leading-7 text-xl text-gray-900">My Bookings</h2>
-          <p className="mt-2 text-m leading-6 text-gray-600">
-            Jimmy Anderson
-          </p>
-    </div>
-    <ul role="list" className="divide-y divide-gray-100 mx-96">
-      {people.map((person) => (
-        <li key={person.email} className="flex justify-between gap-x-6 py-5">
-          <div className="flex gap-x-4">   
-          <p className="text-sm leading-6 text-gray-900">{person.no}</p>
-            <div className="min-w-0 flex-auto">
-              <p className="text-sm font-semibold leading-6 text-gray-900">{person.name}</p>
-              <p className="mt-1 truncate text-xs leading-5 text-gray-500">{person.role}</p>
-            </div>
-          </div>
-          <div className="hidden sm:flex sm:flex-col sm:items-end">
-            <p className="text-sm leading-6 text-gray-900">{person.date}</p>
-            <p className="mt-1 text-xs leading-5 text-gray-500">{person.time}</p>
-          </div>
-          <div className="hidden sm:flex sm:flex-col sm:items-end">
-            <p className="text-sm leading-6 text-gray-900">${person.rate}/hr</p>
-          </div>
-          <div className="hidden sm:flex sm:flex-col sm:items-end">            
-          {person.status==false ?
-          <button class="flex w-full items-center justify-center rounded-md border border-transparent bg-gray-500 py-1 px-3 text-sm font-medium text-white">
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-clock" viewBox="0 0 16 16"> <path d="M8 3.5a.5.5 0 0 0-1 0V9a.5.5 0 0 0 .252.434l3.5 2a.5.5 0 0 0 .496-.868L8 8.71V3.5z"/> <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm7-8A7 7 0 1 1 1 8a7 7 0 0 1 14 0z"/> </svg>
+<div class="container">
+  <h5 class="mt-5 mb-4">My Bookings</h5>
+    <div class="row">
+        <div class="col-12 mb-3 mb-lg-5">
+            <div class="overflow-hidden card table-nowrap table-card">
+                <div class="card-header d-flex align-items-center">
+                    <h6 class="mb-0" className="items-start">Jimmy Anderson</h6>
+                </div>
+                <div class="table-responsive">
+                    <table class="table mb-0">
+                        <thead class="small text-uppercase bg-body text-muted">
+                            <tr>
+                                <th>Name</th>
+                                <th>Service</th>
+                                <th>Date</th>
+                                <th>Time</th>
+                                <th>Status</th>
+                                <th>Cancel</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr class="align-middle">
+                                <td>
+                                    <div class="d-flex align-items-center">
+                                        <img src="https://bootdey.com/img/Content/avatar/avatar1.png" class="avatar sm rounded-pill me-3 flex-shrink-0" alt="Customer"/>
+                                        <div>
+                                            <div class="h6 mb-0 lh-1">Alen John</div>
+                                        </div>
+                                    </div>
+                                </td>
+                                <td>Painter</td>
+                                <td>09 Jul, 2023</td>
+                                <td>15:30</td>
+                                <td><button class="flex w-full items-center justify-center rounded-md border border-transparent bg-gray-500 py-1 w-24 text-sm font-medium text-white">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-clock" viewBox="0 0 16 16"><path d="M8 3.5a.5.5 0 0 0-1 0V9a.5.5 0 0 0 .252.434l3.5 2a.5.5 0 0 0 .496-.868L8 8.71V3.5z"/> <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm7-8A7 7 0 1 1 1 8a7 7 0 0 1 14 0z"/> </svg>
               <span>Pending</span>
-          </button> : 
-          <button class="flex w-full items-center justify-center rounded-md border border-transparent bg-green-600 py-1 px-3 text-sm font-medium text-white">
+          </button></td>
+                                <td class="text-end">
+                                <button 
+                type="submit"           
+                class="flex w-full items-center justify-center rounded-md border border-transparent bg-red-500 py-1 w-24 text-sm font-medium text-white hover:bg-red-400 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
+                  Cancel
+              </button>
+                                </td>
+                            </tr>
+                            <tr class="align-middle">
+                                <td>
+                                    <div class="d-flex align-items-center">
+                                        <img src="https://bootdey.com/img/Content/avatar/avatar2.png" class="avatar sm rounded-pill me-3 flex-shrink-0" alt="Customer"/>
+                                        <div>
+                                            <div class="h6 mb-0 lh-1">Topias Kantola</div>
+                                        </div>
+                                    </div>
+                                </td>
+                                <td>Mechanic</td>
+                                <td>21 Sep, 2023</td>
+                                <td>11:00</td>
+                                <td><button class="flex w-full items-center justify-center rounded-md border border-transparent bg-green-600 py-1 w-24 text-sm font-medium text-white">
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-circle" viewBox="0 0 16 16"> <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/> <path d="M10.97 4.97a.235.235 0 0 0-.02.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-1.071-1.05z"/> </svg>
               <span>Approved</span>
-            </button>}     
-          </div>
-          <div className="hidden sm:flex sm:flex-col sm:items-end">
-            <form class="">
-              <button 
+            </button></td>
+                                <td class="text-end">
+                                <button 
                 type="submit"           
-                class="flex w-full items-center justify-center rounded-md border border-transparent bg-red-500 py-1 px-3 text-sm font-medium text-white hover:bg-red-400 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
+                class="flex w-full items-center justify-center rounded-md border border-transparent bg-red-500 py-1 w-24 text-sm font-medium text-white hover:bg-red-400 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
                   Cancel
-              </button>     
-            </form>
-          </div>
-        </li>
-      ))}
-    </ul>
-
-
-
-
-
-
-
-    
-    
-
-
-
+              </button>
+                                </td>
+                            </tr>
+                            <tr class="align-middle">
+                                <td>
+                                    <div class="d-flex align-items-center">
+                                        <img src="https://bootdey.com/img/Content/avatar/avatar1.png" class="avatar sm rounded-pill me-3 flex-shrink-0" alt="Customer"/>
+                                        <div>
+                                            <div class="h6 mb-0 lh-1">Alen John</div>
+                                        </div>
+                                    </div>
+                                </td>
+                                <td>Electrician</td>
+                                <td>31 May, 2023</td>
+                                <td>11:30</td>
+                                <td><button class="flex w-full items-center justify-center rounded-md border border-transparent bg-gray-500 py-1 w-24 text-sm font-medium text-white">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-clock" viewBox="0 0 16 16"><path d="M8 3.5a.5.5 0 0 0-1 0V9a.5.5 0 0 0 .252.434l3.5 2a.5.5 0 0 0 .496-.868L8 8.71V3.5z"/> <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm7-8A7 7 0 1 1 1 8a7 7 0 0 1 14 0z"/> </svg>
+              <span>Pending</span>
+          </button></td>
+                                <td class="text-end">
+                                <button 
+                type="submit"           
+                class="flex w-full items-center justify-center rounded-md border border-transparent bg-red-500 py-1 w-24 text-sm font-medium text-white hover:bg-red-400 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
+                  Cancel
+              </button>
+                                </td>
+                            </tr>
+                            <tr class="align-middle">
+                                <td>
+                                    <div class="d-flex align-items-center">
+                                        <img src="https://bootdey.com/img/Content/avatar/avatar4.png" class="avatar sm rounded-pill me-3 flex-shrink-0" alt="Customer"/>
+                                        <div>
+                                            <div class="h6 mb-0 lh-1">Wyatt Morris</div>
+                                        </div>
+                                    </div>
+                                </td>
+                                <td>Plumber</td>
+                                <td>20 Aug, 2023</td>
+                                <td>12:45</td>
+                                <td><button class="flex w-full items-center justify-center rounded-md border border-transparent bg-green-600 py-1 w-24 text-sm font-medium text-white">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-circle" viewBox="0 0 16 16"> <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/> <path d="M10.97 4.97a.235.235 0 0 0-.02.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-1.071-1.05z"/> </svg>
+              <span>Approved</span>
+            </button></td>
+                                <td class="text-end">
+                                <button 
+                type="submit"           
+                class="flex w-full items-center justify-center rounded-md border border-transparent bg-red-500 py-1 w-24 text-sm font-medium text-white hover:bg-red-400 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
+                  Cancel
+              </button>
+                                </td>
+                            </tr>
+                            <tr class="align-middle">
+                                <td>
+                                    <div class="d-flex align-items-center">
+                                        <img src="https://bootdey.com/img/Content/avatar/avatar5.png" class="avatar sm rounded-pill me-3 flex-shrink-0" alt="Customer"/>
+                                        <div>
+                                            <div class="h6 mb-0 lh-1">Eliana Stout</div>
+                                        </div>
+                                    </div>
+                                </td>
+                                <td>Electrician</td>
+                                <td>25 Sep, 2023</td>
+                                <td>13:15</td>
+                                <td><button class="flex w-full items-center justify-center rounded-md border border-transparent bg-green-600 py-1 w-24 text-sm font-medium text-white">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-circle" viewBox="0 0 16 16"> <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/> <path d="M10.97 4.97a.235.235 0 0 0-.02.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-1.071-1.05z"/> </svg>
+              <span>Approved</span>
+            </button></td>
+                                <td class="text-end">
+                                <button 
+                type="submit"           
+                class="flex w-full items-center justify-center rounded-md border border-transparent bg-red-500 py-1 w-24 text-sm font-medium text-white hover:bg-red-400 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
+                  Cancel
+              </button>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
     </div>
+</div>
+</div>
+
   )
 }
